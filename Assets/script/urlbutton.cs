a8fb3c835f0b0537a0c769caefdb6a06fd2302f5
wayne688828 <pancurry86@gmail.com>
2022-11-11 09:20:21 +0800

    666

---
+++ Assets/script/urlbutton.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class urlbutton : MonoBehaviour
+{
+    public string Url;
+    public void Open()
+    {
+        Application.OpenURL(Url);
+    }
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+# Unity_CreatorKit_FPS
+ Unity 第一人稱射擊遊戲 Creater Kit
+
+網頁版：
+https://kid421.github.io/Unity_CreatorKit_FPS/web/
